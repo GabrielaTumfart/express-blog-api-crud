a7ad50ed0f349256daed470eb5048e6aa8ddae86
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-03-03 12:46:37 +0100

    feat: aggiungo funzione index nel controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,1 +1,12 @@
 const posts = require("../data/posts");
+
+function index(req, res) {
+  const responseData = {
+    result: posts,
+    message: "Lista dei post di Gabriela",
+    success: true,
+  };
+  res.json(responseData);
+}
+
+module.exports = { index };
