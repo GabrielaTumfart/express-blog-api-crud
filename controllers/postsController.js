b7efdc1b6f7a1bb139a50135a9ee686e3244c8cc
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-03-03 13:18:21 +0100

    feat: aggiungo funzione show nel controller con gestione errore 404

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -9,4 +9,23 @@ function index(req, res) {
   res.json(responseData);
 }
 
-module.exports = { index };
+function show(req, res) {
+  const postId = parseInt(req.params.id);
+  const post = posts.find((posts) => post.id === postId);
+
+  if (!post) {
+    return res.status(404).json({
+      message: `Post ${postID} non trovato`,
+      success: false,
+    });
+  }
+}
+
+const responseData = {
+  result: post,
+  message: `Dettaglio del post ${postId}`,
+  success: true,
+};
+res.json(responseData);
+
+module.exports = { index, show };
