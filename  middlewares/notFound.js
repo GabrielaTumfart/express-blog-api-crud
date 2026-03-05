5dcf6a67e5b8c9c61b13fb76ea724abe32d66138
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-03-05 16:46:51 +0100

    fix: correto errore di sintassi

---  middlewares/notFound.js
+++  middlewares/notFound.js
@@ -1,8 +1,8 @@
-funtion notFound(req, res, next) {
-    res.status(404).json({
-        success: false,
-        message: "Pagina non trovata",
-    });
+function notFound(req, res, next) {
+  res.status(404).json({
+    success: false,
+    message: "Pagina non trovata",
+  });
 }
 
 module.exports = notFound;
