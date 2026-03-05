6ea267ce65845d9fb5b8d7332239f73bc41474a4
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-03-05 17:06:48 +0100

    test: testato middleware notFound su Postman, risponde con status 404. Super!

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,8 +1,8 @@
 function errorsHandler(err, req, res, next) {
-  // * stampo l'errore nel terminale
+  // stampo l'errore nel terminale
   console.log("[ERROR]: " + err.message);
 
-  // * rispondo con status 500 e un messaggio di errore
+  // rispondo con status 500 e un messaggio di errore
   res.status(500).json({
     message: "Internal server error",
     success: false,
